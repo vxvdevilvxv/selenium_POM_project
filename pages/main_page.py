25ce0440bd0e751af3140869c7fed2ab31a6c4dd
Vladimir_Gal'binskiy <gal-vova@ya.ru>
2021-09-05 10:28:08 +0300

    change url, login selector, add page navigation

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
 
 
 class MainPage(BasePage):
@@ -15,4 +14,3 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
-        #return LoginPage(browser=self.browser, url=self.browser.current_url)
