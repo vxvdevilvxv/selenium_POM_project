c055678bf9bd3a4caf528947bfcc513f2ed4067a
Vladimir_Gal'binskiy <gal-vova@ya.ru>
2021-09-05 10:09:35 +0300

    change url, login selector, add page navigation

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
--- test_main_page.py
+++ test_main_page.py
@@ -2,13 +2,15 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    log_page = LoginPage(browser, browser.current_url)
+    log_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
@@ -18,3 +20,4 @@ def test_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
